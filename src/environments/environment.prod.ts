85f63811c36a42eb9b326608571e9216003ccd52
Andreas Glatz <glatzan@gmail.com>
2020-06-15 15:09:41 +0200

    backend config via assets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,18 @@
+import {URLS} from "../assets/config";
+
+export * from '../assets/config';
+
 export const environment = {
   production: true,
-  apiUrl: 'https://sepdetect.com/api'
+
+  _apiUrl: 'https://sepdetect.com/api',
+
+  get apiUrl() {
+    if (!this._apiUrl || this._apiUrl === '') {
+      return URLS.backend;
+    } else {
+      return this._apiUrl;
+    }
+  }
+
 };
